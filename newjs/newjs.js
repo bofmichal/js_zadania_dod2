c531893e3c7702cefbf6191194857160902bb0c0
bofmichal <bofmichal@gmail.com>
2018-06-09 22:20:59 +1000

    zad2

--- newjs/newjs.js
+++ newjs/newjs.js
@@ -9,3 +9,18 @@ function tablica1(){
         console.log(tab1);
         document.getElementById("tab1").innerHTML = tab1;
 }
+//zad 2
+function tablica2(){
+    var tab2=[];
+    tab2[0]=1;
+    tab2[1]=1;
+    //tab2[2]=tab2[0]+tab2[1];
+    for (var i = 0; i < 10; i++) {
+        tab2[i+2] = tab2[i]+tab2[i+1];
+        
+        
+        }
+        
+        console.log(tab2);
+        document.getElementById("tab2").innerHTML = tab2;
+}
